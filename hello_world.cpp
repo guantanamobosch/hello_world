3a8357f1c12833bd9ca525e93002ba9e6bd32e2e
guantanamobosch <luketyson17@gmail.com>
2023-07-27 16:14:33 -0600

    octal, decimal, and hexadecimal manipulators

--- hello_world.cpp
+++ hello_world.cpp
@@ -553,3 +553,23 @@
 
 //     return 0;
 // }
+
+// --------------------------
+// 🛶 Read integral decimal values / generate octal, decimal
+// and hexadecimal output
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int number;
+//     cout << "Please enter an integer: ";
+//     cin >> number;
+//     cout << uppercase
+//          << " octal  \t decimal  \t hexadecimal\n "
+//          << oct << number << "       \t "
+//          << dec << number << "       \t "
+//          << hex << number << endl;
+//     return 0;
+// }
