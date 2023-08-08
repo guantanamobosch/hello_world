6e519564f5368c32869296d2c7289dc5fb22b81f
guantanamobosch <luketyson17@gmail.com>
2023-08-08 11:43:02 -0600

    formatted input with cin.sync(), cin.clear(), and setw()

--- hello_world.cpp
+++ hello_world.cpp
@@ -680,3 +680,35 @@
 
 //     return 0;
 // }
+
+// ---------------------------
+// 🛶 Formatted input with cin.sync(), cin.clear(), and setw()
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string label;
+    double price;
+
+    cout << "\nPlease enter an article label: ";
+
+    cin >> setw(16);
+    cin >> label;
+
+    cin.sync();
+    cin.clear();
+
+    cout << "\nEnter the price of the article: ";
+    cin >> price;
+
+    cout << fixed << setprecision(2)
+         << "\nArticle:"
+         << "\n  Label:  " << label
+         << "\n  Price:  " << price << endl;
+
+    return 0;
+}
